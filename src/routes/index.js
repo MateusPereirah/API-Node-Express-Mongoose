1c16ce888e5da185c6e305cae2f4b094ae3f39de
MateusPereirah <mateus.pereira1@outlook.com.br>
2024-07-07 05:49:34 -0300

    Mudança com middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,12 @@ import livros from "./livrosRoutes.js"
 import autores from "./autoresRotas.js"
 
 const routes = (app) => {
-    app.route("/").get((req,res) => res.status(200).send("Curso de Node.js"))
+    app.route("/").get((req,res) => res.status(200).send("Curso de Node.js"));
+
+    app.use((req, res, next) => {
+        console.log('Middleware global executado!');
+        next();
+    });
 
     app.use(express.json(), livros, autores)
 };
